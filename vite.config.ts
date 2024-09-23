0657dece190b4e1fdce65f06bdaf7b847fe30d2f
MarcosKern <vkg.marcos@gmail.com>
2024-09-23 19:34:07 -0300

    update TS config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "../"),
+      "@server": path.resolve(__dirname, "../server/src"),
+    },
+  },
+  plugins: [react()],
+});
